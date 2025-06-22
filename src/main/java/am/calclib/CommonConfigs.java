3d1592aabf9d4181b1a72b3d7b08b62cb18ceddb
kei <keisamurai@gmail.com>
2025-06-22 14:46:12 +0900

    first commit

---
+++ src/main/java/am/calclib/CommonConfigs.java
@@ -0,0 +1,8 @@
+package am.calclib;
+
+import java.math.RoundingMode;
+
+public class CommonConfigs {
+    public static final int DEFAULT_SCALE = 4;
+    public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_EVEN;
+}
--- src/test/java/am/calclib/CalcInterestTest.java
+++ src/test/java/am/calclib/CalcInterestTest.java
@@ -1,6 +1,8 @@
 package am.calclib;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static am.calclib.CommonConfigs.DEFAULT_SCALE;
+import static am.calclib.CommonConfigs.ROUNDING_MODE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
@@ -10,7 +12,10 @@ public class CalcInterestTest {
     @Test
     public void calculateInterest_success() {
         BigDecimal rate = new BigDecimal("0.05");
-        assertTrue(c.calculateInterest(rate, 10).compareTo(new BigDecimal("0.50")) == 0);
+        BigDecimal expected = new BigDecimal("0.50");
+        BigDecimal actual = c.calculateInterest(rate, 10);
+
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -18,8 +23,12 @@ public class CalcInterestTest {
         BigDecimal rate = new BigDecimal("0.05");
         Integer term = 10;
 
-        double result = c.calculateCompoundInterest(rate, term).setScale(3, BigDecimal.ROUND_UP).doubleValue();
-        assertTrue(result == 0.629);
+        BigDecimal compoundInterest = c.calculateCompoundInterest(rate, term);
+        BigDecimal rounded = compoundInterest.setScale(DEFAULT_SCALE, ROUNDING_MODE);
+        double expected = 0.6289;
+        double actual = rounded.doubleValue();
+
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -27,7 +36,11 @@ public class CalcInterestTest {
         BigDecimal rate = new BigDecimal("0.05");
         Integer term = 20;
 
-        double result = c.calculateCompoundInterest(rate, term).setScale(3, BigDecimal.ROUND_UP).doubleValue();
-        assertTrue(result == 1.654);
+        BigDecimal compoundInterest = c.calculateCompoundInterest(rate, term);
+        BigDecimal rounded = compoundInterest.setScale(DEFAULT_SCALE, ROUNDING_MODE);
+        double expected = 1.6533;
+        double actual = rounded.doubleValue();
+
+        assertEquals(expected, actual);
     }
 }
